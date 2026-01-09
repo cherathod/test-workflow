9ac52a05694549e2cfa36caa7069cec70ebf2f08
cherathod <chetanrathodins03@gmail.com>
2026-01-09 17:49:30 +0530

    updating test file

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,9 +1,7 @@
 from src.math_operathion import add, sub
 
 def test_add():
-    assert add(1 + 2) == 3
-    assert add(0 + 5) == 5
+    assert add(1, 2) == 3
 
 def test_sub():
-    assert sub(10 - 20) == -10
-    assert sub(5 - 2) == 3
+    assert sub(10, 20) == -10
